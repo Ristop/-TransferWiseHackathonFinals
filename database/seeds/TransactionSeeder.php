db0b00fff5fc8c38d96f4ab1c1a207f184a972f4
Mart M <repogfreow@gmail.com>
2016-04-23 02:15:55 +0300

    dummy data seeding

---
+++ database/seeds/TransactionSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TransactionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('transactions')->insert([
+            'source' => 'USD',
+            'target' => 'EUR',
+            'sourceAmount' => '116.62',
+            'targetAmount' => '100',
+            'rate' => '0.88015',
+            'fee' => '3'
+        ]);
+
+        DB::table('transactions')->insert([
+            'source' => 'GBP',
+            'target' => 'EUR',
+            'sourceAmount' => '1000',
+            'targetAmount' => '1266.51',
+            'rate' => '1.27285',
+            'fee' => '4.98'
+        ]);
+
+        DB::table('transactions')->insert([
+            'source' => 'EUR',
+            'target' => 'SEK',
+            'sourceAmount' => '109.81',
+            'targetAmount' => '1000',
+            'rate' => '9.2758667',
+            'fee' => '2'
+        ]);
+    }
+}
