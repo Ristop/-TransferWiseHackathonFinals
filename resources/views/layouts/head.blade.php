371a349fea661c5f479c2830d358cb9b715b5349
Risto Pärnapuu <ristoparnapuu@gmail.com>
2016-04-23 01:21:21 +0300

    some stuff

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,18 @@
+<meta charset="UTF-8">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="_token"content="<?php echo csrf_token()?>"/>
+
+<title>Valgeranna puhkekeskus</title>
+
+<!--stylesheets-->
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<link rel="stylesheet" href={{URL::asset('/css/app.css')}}>
+
+<!--custom fonts-->
+<link href='https://fonts.googleapis.com/css?family=Roboto:400' rel='stylesheet' type='text/css'>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-virtual-handshake">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">VirtualHandshake</a>
+        </div>
+
+    </div><!-- /.container-fluid -->
+</nav>
