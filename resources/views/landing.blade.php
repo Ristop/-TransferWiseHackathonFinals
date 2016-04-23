1fdb7ea41e0102df162ce8886940579fa94f54a6
Risto Pärnapuu <ristoparnapuu@gmail.com>
2016-04-23 04:36:51 +0300

    landing page stuff

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,9 +1,25 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="container">
-        <div class="content">
-            <div class="title">Laravel 5</div>
+    <div class="section-main-first">
+        <div class="img-responsive">
+            <img src="img/sample2.jpg">
+        </div>
+        <div class="section-main-first-content">
+            <div class="Buyer col-md-6 col-xs-6">
+                <p class="pull-right">Client</p>
+            </div>
+            <div class="Vendor col-md-6 col-xs-6">
+                <p class="pull-left">Vendor</p>
+            </div>
+        </div>
+        <div class="section-main-second-content">
+            <div class="Buyer col-md-6 col-xs-6">
+                <p class="pull-right"></p>
+            </div>
+            <div class="Vendor col-md-6 col-xs-6">
+                <p class="pull-left"></p>
+            </div>
         </div>
     </div>
 @stop
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,9 +1,5 @@
 <nav class="navbar navbar-virtual-handshake">
     <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <a class="navbar-brand" href="#">VirtualHandshake</a>
-        </div>
-
-    </div><!-- /.container-fluid -->
+        <p class="text-center">Virtual Handshake</p>
+    </div>
 </nav>
