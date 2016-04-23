729b1a9b18ddd210ca4ededc126d773cf9cb4fb9
Mart M <repogfreow@gmail.com>
2016-04-23 12:39:42 +0300

    merge branches

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -44,7 +44,7 @@
                     Client
                 </h3>
                 <p class="steps-description">
-                    The customer wires their transfer to our escrow account.
+                    Our client saves time and money by using our service.
                 </p>
             </div>
 
@@ -126,11 +126,14 @@
                 </div>
                 <div class="timeline-content">
                     <div class="blue">
-                        <h2>Client buys the item form the vendor.</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Atque, facilis quo maiores magnam modi ab libero praesentium blanditiis.
-                        </p>
+                        <h2>The customer wires their transfer to our escrow account.</h2>
+                        <p>Benefits:</p>
+                        <ul>
+                            <li>Shorter wait time for delivery</li>
+                            <li>Assurance on shipment delivery</li>
+                            <li>Cutting costs on currency conversion</li>
+                            <li>Faster currency conversion</li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -154,13 +157,11 @@
                 </div>
                 <div class="timeline-content right">
                     <div class="black">
-                        <h2>LOREM IPSUM DOLOR</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, facilis quo. Maiores magnam
-                            modi ab libero praesentium blanditiis consequatur aspernatur accusantium maxime molestiae
-                            sunt
-                            ipsa.
-                        </p>
+                        <h2>The funds are waiting for confirmation in our escrow account.</h2>
+                        <p>The client can choose for how long the money is kept on our escrow account, waiting for
+                            confirmation. When confirmation is given by the vendor that the product has been shipped, we
+                            release the escrow payment.</p>
+
                     </div>
                 </div>
             </div>
@@ -181,12 +182,37 @@
                 </div>
                 <div class="timeline-content">
                     <div class="blue">
-                        <h2>LOREM IPSUM DOLOR</h2>
+                        <h2>The customer waits for the product to arrive.</h2>
                         <p>
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, facilis quo. Maiores magnam
                             modi ab libero praesentium blanditiis consequatur aspernatur accusantium maxime molestiae
                             sunt
                             ipsa.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="timeline-item">
+                <div class="timeline-icon">
+                    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                         xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                         width="21px" height="20px" viewBox="0 0 21 20" enable-background="new 0 0 21 20"
+                         xml:space="preserve">
+                <path fill="#FFFFFF" d="M17.92,3.065l-1.669-2.302c-0.336-0.464-0.87-0.75-1.479-0.755C14.732,0.008,7.653,0,7.653,0v5.6
+                        c0,0.096-0.047,0.185-0.127,0.237c-0.081,0.052-0.181,0.06-0.268,0.02l-1.413-0.64C5.773,5.183,5.69,5.183,5.617,5.215l-1.489,0.65
+                        c-0.087,0.038-0.19,0.029-0.271-0.023c-0.079-0.052-0.13-0.141-0.13-0.235V0H2.191C1.655,0,1.233,0.434,1.233,0.97
+                        c0,0,0.025,15.952,0.031,15.993c0.084,0.509,0.379,0.962,0.811,1.242l2.334,1.528C4.671,19.905,4.974,20,5.286,20h10.307
+                        c1.452,0,2.634-1.189,2.634-2.64V4.007C18.227,3.666,18.12,3.339,17.92,3.065z M16.42,17.36c0,0.464-0.361,0.833-0.827,0.833H5.341
+                        l-1.675-1.089h10.341c0.537,0,0.953-0.44,0.953-0.979V2.039l1.459,2.027V17.36L16.42,17.36z"/>
+                </svg>
+
+                </div>
+                <div class="timeline-content right">
+                    <div class="black">
+                        <h2>Escrow release</h2>
+                        <p>
+                            When confirmation is given by the customer that the product has been shipped, we release the escrow payment to be delivered to the vendor.
                         </p>
                     </div>
                 </div>
